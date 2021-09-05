c55fe3d632c276a77a71e0f57a0eeb25ef6d8153
Wil <wilhelmjanmapogi@gmail.com>
2021-09-05 12:10:54 +0800

    Adjust spaces

--- script.js
+++ script.js
@@ -8,27 +8,27 @@ const GameBoard = (function() {
 })();
 
 const displayController = (function() {
-
     const _makeDiv = (tile) => {
         const div = document.createElement('div');
         div.classList.add('tile');
         div.textContent = tile;
         return div
     }
+
     const gameBoardDOM = document.querySelector('.game-board');
-    console.log(gameBoardDOM);
     const renderBoard = (board) => {
         board.forEach(tile => {
             const tileDiv = _makeDiv(tile);
             gameBoardDOM.appendChild(tileDiv);
         });
-
     }
 
 
     return { renderBoard }
 })();
-console.log(displayController);
+
+
+
 displayController.renderBoard(GameBoard.getBoard());
 
 
