13049422ff44b1243b4b2a0124482ab174c50e4c
Wil <wilhelmjanmapogi@gmail.com>
2021-09-06 08:23:09 +0800

    Add changing of turn on players

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const GameBoard = (function() {
     let _gameTiles = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
     const getBoard = () => _gameTiles;
+    const changeLetter = () => {
 
+    };
     return { getBoard, }
 
 
 })();
 
-const displayController = (function() {
+const gameControl = (function() {
     const _makeDiv = (tile) => {
         const div = document.createElement('div');
         div.classList.add('tile');
@@ -28,26 +30,40 @@ const displayController = (function() {
 })();
 
 
-
-displayController.renderBoard(GameBoard.getBoard());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Player Factory function
-const Player = function(name) {
-    const getName = () => name
+const Player = function(name, symbol, bool) {
+    let _score = 0;
+    const marker = symbol;
+    let _isMyTurn = bool;
+    const changeMyTurn = () => {
+        _isMyTurn = (_isMyTurn) ? false : true
+    };
+    const getMyTurn = () => _isMyTurn
+    const getScore = () => _score;
+    const getName = () => name;
 
-    return { getName }
+    return { marker, getName, getScore, changeMyTurn, getMyTurn }
 };
+
+const player1 = Player('wil', '❌', true);
+const player2 = Player('Bot', '⭕', false);
+
+
+
+gameControl.renderBoard(GameBoard.getBoard());
+const gameBoardTiles = document.querySelectorAll('.tile');
+
+const whichPlayerTurn = (p1, p2) => (!!p1) ? 1 : 0;
+const changePlayerTurns = () => {
+    player1.changeMyTurn();
+    player2.changeMyTurn();
+}
+
+gameBoardTiles.forEach(tile => {
+    tile.addEventListener('click', (e) => {
+        const mark = (whichPlayerTurn(player1.getMyTurn(), player2.getMyTurn())) ? player1.marker : player2.marker;
+        e.target.textContent = mark;
+        changePlayerTurns();
+
+    });
+})
