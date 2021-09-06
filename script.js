003cecf514428a30b9815505b189eb47e29fd501
Wil <wilhelmjanmapogi@gmail.com>
2021-09-06 09:12:14 +0800

    Disable click event after been set

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 const GameBoard = (function() {
     let _gameTiles = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
     const getBoard = () => _gameTiles;
-    const changeLetter = () => {
-
+    const changeTile = (n, mark) => {
+        _gameTiles.splice(n, 1, mark);
+        console.log(_gameTiles);
     };
-    return { getBoard, }
-
+    return { getBoard, changeTile }
 
 })();
 
 const gameControl = (function() {
-    const _makeDiv = (tile) => {
+    const _makeDiv = (tile, arrNum) => {
         const div = document.createElement('div');
         div.classList.add('tile');
+        div.setAttribute('data-array-number', arrNum);
         div.textContent = tile;
         return div
     }
 
     const gameBoardDOM = document.querySelector('.game-board');
     const renderBoard = (board) => {
-        board.forEach(tile => {
-            const tileDiv = _makeDiv(tile);
+        for (let i = 0; i < board.length; i++) {
+            const tileDiv = _makeDiv(board[i], i);
             gameBoardDOM.appendChild(tileDiv);
-        });
+        }
     }
 
 
@@ -50,8 +51,6 @@ const player2 = Player('Bot', '⭕', false);
 
 
 
-gameControl.renderBoard(GameBoard.getBoard());
-const gameBoardTiles = document.querySelectorAll('.tile');
 
 const whichPlayerTurn = (p1, p2) => (!!p1) ? 1 : 0;
 const changePlayerTurns = () => {
@@ -59,11 +58,22 @@ const changePlayerTurns = () => {
     player2.changeMyTurn();
 }
 
+
+gameControl.renderBoard(GameBoard.getBoard());
+
+const gameBoardTiles = Array.from(document.querySelectorAll('div.tile'));
 gameBoardTiles.forEach(tile => {
     tile.addEventListener('click', (e) => {
-        const mark = (whichPlayerTurn(player1.getMyTurn(), player2.getMyTurn())) ? player1.marker : player2.marker;
+        const p1Turn = player1.getMyTurn(),
+            p2Turn = player2.getMyTurn();
+        const mark = (whichPlayerTurn(p1Turn, p2Turn)) ? player1.marker : player2.marker;
         e.target.textContent = mark;
+        // disables click event on the css
+        e.target.classList.add('clicked');
+        console.log(e.target);
         changePlayerTurns();
+        const tileNum = e.target.getAttribute('data-array-number');
+        GameBoard.changeTile(tileNum, mark);
 
     });
-})
+});
